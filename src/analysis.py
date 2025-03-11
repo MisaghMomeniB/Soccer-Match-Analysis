47d535b6abd2c9e3e81a24ead41c8ffc09924d37
MisaghMomeniB <jenabemisagh@gmail.com>
2025-03-11 22:08:19 +0330

    import

--- src/analysis.py
+++ src/analysis.py
@@ -62,3 +62,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
+
+# Model Building and Prediction
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
