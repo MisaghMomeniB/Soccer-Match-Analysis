3fbf86a8bc779bbe4b42ac5bbf2c653e1e98af1f
MisaghMomeniB <jenabemisagh@gmail.com>
2025-03-11 22:07:55 +0330

    Standardizing the feature data (important for many models)

--- src/analysis.py
+++ src/analysis.py
@@ -57,3 +57,8 @@ y = df['result']
 
 # Splitting the data into training and test sets (80% train, 20% test)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Standardizing the feature data (important for many models)
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
